d3747bb5440050abfa2b6090014dd30466352220
mescoda <mescodasun@gmail.com>
2014-03-25 13:19:51 +0800

    es5 first blood

---
+++ src/lang/es5.js
@@ -0,0 +1,208 @@
+define(['koko/lang/type'], function(Type) {
+    var json = {},
+        object = {},
+        string = {},
+        array = {};
+
+    var objectPrototype = Object.prototype,
+        stringPrototype = String.prototype,
+        arrayPrototype = Array.prototype;
+
+    // json
+    json.parse = function(str) {
+        if(window.JSON && window.JSON.parse) {
+            return window.JSON.parse(str);
+        }
+        if(str && typeof str === 'string') {
+            return (new Function("return (" + str + ")"))();
+        }
+        return null;
+    }
+    json.stringify = function(obj) {
+        if(window.JSON && window.JSON.stringify) {
+            return window.JSON.stringify(obj);
+        }
+    };
+
+    // object
+    object.keys = function(obj) {
+        if(!Type.isObject(obj)) {
+            return [];
+        }
+        if(Object.keys) {
+            return Object.keys(obj);
+        }
+        var keys = [];
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key)) {
+                keys.push(key);
+            }
+        }
+        return keys;
+    };
+
+    object.create = function() {
+
+    };
+
+    object.getPrototypeOf = function(obj) {
+
+    };
+
+    // string
+    string.trim = function(str) {
+        var trimRegex = /^\s+|\s+$/g,
+            trimProto = stringPrototype.trim;
+        if(trimProto) {
+            return str == null ? '' : trimProto.call(str);
+        }
+        return str == null ? '' : String(str).replace(trimRegex, '');
+    };
+
+    // array
+    array.indexOf = function(array, item, fromIndex) {
+        var indexOfProto = arrayPrototype.indexOf,
+            length = array.length,
+            i;
+        if(length === 0) {
+            return -1;
+        }
+        fromIndex = +fromIndex || 0;
+        i = fromIndex < 0 ? Math.max(0, fromIndex + length) : fromIndex;
+        if(indexOfProto) {
+            return indexOfProto.call(array, item, i);
+        }
+        for(; i < length; i++) {
+            if(array[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
+    array.lastIndexOf = function(array, item, fromIndex) {
+        var lastIndexOfProto = arrayPrototype.lastIndexOf,
+            length = array.length,
+            i;
+        if(length === 0) {
+            return -1;
+        }
+        // not a number or gt length
+        fromIndex = Math.min(+fromIndex || length - 1, length - 1);
+        i = fromIndex < 0 ? fromIndex + length : fromIndex;
+        if(lastIndexOfProto) {
+            return lastIndexOfProto.call(array, item, i);
+        }
+        for(; i > -1; i--) {
+            if(array[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
+    array.forEach = function(arr, iterator, context) {
+        var forEachProto = arrayPrototype.forEach;
+        if(forEachProto) {
+            forEachProto.call(arr, iterator, context);
+        } else {
+            for(var i = 0, iLen = arr.length; i < iLen; i++) {
+                if(i in arr) {
+                    iterator.call(context, arr[i], i, arr);
+                }
+            }
+        }
+
+    };
+
+    array.map = function(arr, iterator, context) {
+        var mapProto = arrayPrototype.map,
+            result = [];
+        if(mapProto) {
+            return mapProto.call(arr, iterator, context);
+        }
+        for(var i = 0, iLen = arr.length; i < iLen; i++) {
+            if(i in arr) {
+                result[i] = (iterator.call(context, arr[i], i, arr));
+            }
+        }
+        return result;
+    };
+
+    array.filter = function(arr, iterator, context) {
+        var filterProto = arrayPrototype.filter,
+            result = [];
+        if(filterProto) {
+            return filterProto.call(arr, iterator, context);
+        }
+        for(var i = 0, iLen = arr.length; i < iLen; i++) {
+            if(i in arr && iterator.call(context, arr[i], i, arr)) {
+                result.push(arr[i]);
+            }
+        }
+        return result;
+    };
+
+    array.every = function(arr, iterator, context) {
+        var everyProto = arrayPrototype.every;
+        if(everyProto) {
+            return everyProto.call(arr, iterator, context);
+        }
+        for(var i = 0, iLen = arr.length; i < iLen; i++) {
+            if(i in arr && !iterator.call(context, arr[i], i, arr)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    array.some = function(arr, iterator, context) {
+        var someProto = arrayPrototype.some;
+        if(someProto) {
+            return someProto.call(arr, iterator, context);
+        }
+        for(var i = 0, iLen = arr.length; i < iLen; i++) {
+            if(i in arr && iterator.call(context, arr[i], i, arr)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    array.reduce = function(arr, iterator, initialValue) {
+        var reduceProto = arrayPrototype.reduce,
+            prev,
+            i,
+            length = arr.length,
+            setPrev = false;
+        if(reduceProto) {
+            return initialValue ? reduceProto.call(arr, iterator, initialValue) : reduceProto.call(arr, iterator);
+        }
+        if(!initialValue && length === 0) {
+            throw new TypeError('Reduce of empty array with no initial value');
+        }
+        if(initialValue) {
+            prev = initialValue;
+            setPrev = true;
+        }
+        for(i = 0; i < length; i++) {
+            if(i in arr) {
+                if(setPrev) {
+                    prev = iterator.call(arr, prev, arr[i], i, arr);
+                } else {
+                    prev = arr[i]
+                    setPrev = true;
+                }
+            }
+        }
+        return prev;
+    };
+
+    return {
+        json: json,
+        object: object,
+        string: string,
+        array: array
+    };
+
+});
