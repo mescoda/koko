eb37e9414ea3a87e4fe74cc817a99fe0bf75d3d2
mescoda <mescodasun@gmail.com>
2015-03-05 16:54:21 +0800

    conform to the code style

--- src/lang/es5.js
+++ src/lang/es5.js
@@ -1,228 +1,355 @@
-define(['koko/lang/generic'], function(generic) {
-    var json = {},
-        object = {},
-        array = {},
-        fn = {},
-        string = {};
+define(function (require) {
+    'use strict';
 
-    var objectPrototype = Object.prototype,
-        functionPrototype = Function.prototype,
-        stringPrototype = String.prototype,
-        arrayPrototype = Array.prototype;
+    /**
+     * es5 functions of koko
+     *
+     * @exports es5
+     */
+    var exports = {};
 
-    // json
-    json.parse = function(str) {
-        if(window.JSON && window.JSON.parse) {
+    exports.json = {};
+    exports.object = {};
+    exports.array = {};
+    exports.fn = {};
+    exports.string = {};
+
+    var generic = require('./generic');
+
+    var functionPrototype = Function.prototype;
+    var stringPrototype = String.prototype;
+    var arrayPrototype = Array.prototype;
+
+
+    /**
+     * json.parse
+     *
+     * @method parse
+     * @param {string} str target string
+     * @return {*} result
+     */
+    exports.json.parse = function (str) {
+        if (window.JSON && window.JSON.parse) {
             return window.JSON.parse(str);
         }
-        if(str && typeof str === 'string') {
-            return (new Function("return (" + str + ")"))();
+        if (str && typeof str === 'string') {
+            return (new Function('return (' + str + ')'))();
         }
         return null;
-    }
-    json.stringify = function(obj) {
-        if(window.JSON && window.JSON.stringify) {
-            return window.JSON.stringify(obj);
-        }
     };
 
-    // object
-    object.keys = function(obj) {
-        if(Object.keys) {
+
+    /**
+     * get keys of an object
+     *
+     * @method keys
+     * @param {Object} obj object to be got
+     * @return {Array.<string>} the keys
+     */
+    exports.keys = exports.object.keys = function (obj) {
+        if (Object.keys) {
             return Object.keys(obj);
         }
+
         var keys = [];
-        generic.forInOwn(obj, function(value, key) {
-            keys.push(key)
+        generic.forInOwn(obj, function (value, key) {
+            keys.push(key);
         });
         return keys;
     };
 
-    object.create = function(obj) {
+
+    /**
+     * simple recreate for Object.create
+     *
+     * @method create
+     * @param {Object} obj target object
+     * @return {Object}
+     */
+    exports.create = exports.object.create = function (obj) {
         function F() {}
         F.prototype = obj;
         return new F();
     };
 
-    object.getPrototypeOf = function(obj) {
-        var getPrototypeOfProto = objectPrototype.getPrototypeOf;
-        if(getPrototypeOfProto) {
-            return getPrototypeOfProto.call(obj);
-        } else {
-            return obj.__proto__ || obj.constructor.prototype;
-        }
-    };
 
-    // function
-    fn.bind = function(fn, context) {
-        var bindProto = functionPrototype.bind,
-            bindArgs = arrayPrototype.slice.call(arguments, 2),
-            newObj = function() {},
-            bound;
-        if(bindProto) {
+    /**
+     * recreate of Function.prototype.bind
+     *
+     * @method bind
+     * @param {Function} fn function to be bound
+     * @param {Object} context the context
+     * @return {Function} function after being bound
+     */
+    exports.bind = exports.fn.bind = function (fn, context) {
+        var bindProto = functionPrototype.bind;
+
+        if (bindProto) {
             return bindProto.apply(fn, arrayPrototype.slice.call(arguments, 1));
-        } else {
-            bound = function() {
-                var args = bindArgs.concat( arrayPrototype.slice.call(arguments) ),
-                    newInstance,
-                    result;
-                if(!(this instanceof bound)) {
-                    return fn.apply(context, args);
-                }
-                newObj.prototype = fn.prototype;
-                newInstance = new newObj;
-                result = fn.apply(newInstance, args);
-                if(Object(result) === result) {
-                    return result;
-                }
-                return newInstance;
-            };
-            return bound;
         }
+
+        var bindArgs = arrayPrototype.slice.call(arguments, 2);
+        var Temp = function () {};
+        var boundFn = function () {
+            var args = bindArgs.concat(arrayPrototype.slice.call(arguments));
+            if (!(this instanceof boundFn)) {
+                return fn.apply(context, args);
+            }
+
+            Temp.prototype = fn.prototype;
+            var newInstance = new Temp();
+            var result = fn.apply(newInstance, args);
+            if (Object(result) === result) {
+                return result;
+            }
+            return newInstance;
+        };
+        return boundFn;
     };
 
-    // string
-    string.trim = function(str) {
-        var trimRegex = /^[\s\xA0\u3000\uFEFF]+|[\s\xA0\u3000\uFEFF]+$/g,
-            trimProto = stringPrototype.trim;
-        if(trimProto) {
+
+    /**
+     * recreate of String.prototype.trim
+     *
+     * @method trim
+     * @param {string} str string to be trimed
+     * @return {string} string after being trimed
+     */
+    exports.trim = exports.string.trim = function (str) {
+        var trimRegex = /^[\s\xA0\u3000\uFEFF]+|[\s\xA0\u3000\uFEFF]+$/g;
+        var trimProto = stringPrototype.trim;
+
+        if (trimProto) {
             return str == null ? '' : trimProto.call(str);
         }
+
         return str == null ? '' : String(str).replace(trimRegex, '');
     };
 
-    // array
-    array.indexOf = function(array, item, fromIndex) {
-        var indexOfProto = arrayPrototype.indexOf,
-            length = array.length,
-            i;
-        if(length === 0) {
+
+    /**
+     * recreate of Array.prototype.indexOf
+     *
+     * @method indexOf
+     * @param {Array} array target array
+     * @param {*} item item to be checked
+     * @param {number=} fromIndex the fromIndex
+     * @return {number} if found return the index of item else return -1
+     */
+    exports.indexOf = exports.array.indexOf = function (array, item, fromIndex) {
+        var indexOfProto = arrayPrototype.indexOf;
+        var length = array.length;
+
+        if (length === 0) {
             return -1;
         }
+
         fromIndex = +fromIndex || 0;
-        i = fromIndex < 0 ? Math.max(0, fromIndex + length) : fromIndex;
-        if(indexOfProto) {
+
+        var i = fromIndex < 0 ? Math.max(0, fromIndex + length) : fromIndex;
+
+        if (indexOfProto) {
             return indexOfProto.call(array, item, i);
         }
-        for(; i < length; i++) {
-            if(i in array && array[i] === item) {
+
+        for (; i < length; i++) {
+            if (i in array && array[i] === item) {
                 return i;
             }
         }
         return -1;
     };
 
-    array.lastIndexOf = function(array, item, fromIndex) {
-        var lastIndexOfProto = arrayPrototype.lastIndexOf,
-            length = array.length,
-            i;
-        if(length === 0) {
+
+    /**
+     * recreate of Array.prototype.lastIndexOf
+     *
+     * @method lastIndexOf
+     * @param {Array} array target array
+     * @param {*} item item to be checked
+     * @param {number=} fromIndex the fromIndex
+     * @return {number} if found return the last index of item else return -1
+     */
+    exports.lastIndexOf = exports.array.lastIndexOf = function (array, item, fromIndex) {
+        var lastIndexOfProto = arrayPrototype.lastIndexOf;
+        var length = array.length;
+
+        if (length === 0) {
             return -1;
         }
+
         // not a number or gt length
         fromIndex = Math.min(+fromIndex || length - 1, length - 1);
-        i = fromIndex < 0 ? fromIndex + length : fromIndex;
-        if(lastIndexOfProto) {
+
+        var i = fromIndex < 0 ? fromIndex + length : fromIndex;
+
+        if (lastIndexOfProto) {
             return lastIndexOfProto.call(array, item, i);
         }
-        for(; i > -1; i--) {
-            if(i in array && array[i] === item) {
+
+        for (; i > -1; i--) {
+            if (i in array && array[i] === item) {
                 return i;
             }
         }
         return -1;
     };
 
-    array.forEach = function(arr, iterator, context) {
+
+    /**
+     * recreate of Array.prototype.forEach
+     *
+     * @method forEach
+     * @param {Array} arr target array
+     * @param {Function} iterator the iterator function
+     * @param {Object=} context iterator context
+     */
+    exports.forEach = exports.array.forEach = function (arr, iterator, context) {
         var forEachProto = arrayPrototype.forEach;
-        if(forEachProto) {
+
+        if (forEachProto) {
             forEachProto.call(arr, iterator, context);
         } else {
-            for(var i = 0, iLen = arr.length; i < iLen; i++) {
-                if(i in arr) {
+            for (var i = 0, iLen = arr.length; i < iLen; i++) {
+                if (i in arr) {
                     iterator.call(context, arr[i], i, arr);
                 }
             }
         }
-
     };
 
-    array.map = function(arr, iterator, context) {
-        var mapProto = arrayPrototype.map,
-            result = [];
-        if(mapProto) {
+
+    /**
+     * recreate of Array.prototype.map
+     *
+     * @method map
+     * @param {Array} arr target array
+     * @param {Function} iterator the iterator function
+     * @param {Object=} context iterator context
+     */
+    exports.map = exports.array.map = function (arr, iterator, context) {
+        var mapProto = arrayPrototype.map;
+
+        if (mapProto) {
             return mapProto.call(arr, iterator, context);
         }
-        for(var i = 0, iLen = arr.length; i < iLen; i++) {
-            if(i in arr) {
+
+        var result = [];
+        for (var i = 0, iLen = arr.length; i < iLen; i++) {
+            if (i in arr) {
                 result[i] = (iterator.call(context, arr[i], i, arr));
             }
         }
         return result;
     };
 
-    array.filter = function(arr, iterator, context) {
-        var filterProto = arrayPrototype.filter,
-            result = [];
-        if(filterProto) {
+
+    /**
+     * recreate of Array.prototype.filter
+     *
+     * @method filter
+     * @param {Array} arr target array
+     * @param {Function} iterator the iterator function
+     * @param {Object=} context iterator context
+     */
+    exports.filter = exports.array.filter = function (arr, iterator, context) {
+        var filterProto = arrayPrototype.filter;
+
+        if (filterProto) {
             return filterProto.call(arr, iterator, context);
         }
-        for(var i = 0, iLen = arr.length; i < iLen; i++) {
-            if(i in arr && iterator.call(context, arr[i], i, arr)) {
+
+        var result = [];
+        for (var i = 0, iLen = arr.length; i < iLen; i++) {
+            if (i in arr && iterator.call(context, arr[i], i, arr)) {
                 result.push(arr[i]);
             }
         }
         return result;
     };
 
-    array.every = function(arr, iterator, context) {
+
+    /**
+     * recreate of Array.prototype.every
+     *
+     * @method every
+     * @param {Array} arr target array
+     * @param {Function} iterator the iterator function
+     * @param {Object=} context iterator context
+     */
+    exports.every = exports.array.every = function (arr, iterator, context) {
         var everyProto = arrayPrototype.every;
-        if(everyProto) {
+
+        if (everyProto) {
             return everyProto.call(arr, iterator, context);
         }
-        for(var i = 0, iLen = arr.length; i < iLen; i++) {
-            if(i in arr && !iterator.call(context, arr[i], i, arr)) {
+
+        for (var i = 0, iLen = arr.length; i < iLen; i++) {
+            if (i in arr && !iterator.call(context, arr[i], i, arr)) {
                 return false;
             }
         }
         return true;
     };
 
-    array.some = function(arr, iterator, context) {
+
+    /**
+     * recreate of Array.prototype.some
+     *
+     * @method some
+     * @param {Array} arr target array
+     * @param {Function} iterator the iterator function
+     * @param {Object=} context iterator context
+     */
+    exports.some = exports.array.some = function (arr, iterator, context) {
         var someProto = arrayPrototype.some;
-        if(someProto) {
+
+        if (someProto) {
             return someProto.call(arr, iterator, context);
         }
-        for(var i = 0, iLen = arr.length; i < iLen; i++) {
-            if(i in arr && iterator.call(context, arr[i], i, arr)) {
+
+        for (var i = 0, iLen = arr.length; i < iLen; i++) {
+            if (i in arr && iterator.call(context, arr[i], i, arr)) {
                 return true;
             }
         }
         return false;
     };
 
-    array.reduce = function(arr, iterator, initialValue) {
-        var reduceProto = arrayPrototype.reduce,
-            prev,
-            i,
-            length = arr.length,
-            setPrev = false;
-        if(reduceProto) {
+
+    /**
+     * recreate of Array.prototype.reduce
+     *
+     * @method reduce
+     * @param {Array} arr target array
+     * @param {Function} iterator the iterator function
+     * @param {*} initialValue initialValue
+     */
+    exports.reduce = exports.array.reduce = function (arr, iterator, initialValue) {
+        var reduceProto = arrayPrototype.reduce;
+
+        if (reduceProto) {
             return initialValue ? reduceProto.call(arr, iterator, initialValue) : reduceProto.call(arr, iterator);
         }
-        if(!initialValue && length === 0) {
+
+        var length = arr.length;
+        if (!initialValue && length === 0) {
             throw new TypeError('Reduce of empty array with no initial value');
         }
-        if(initialValue) {
+
+        var prev;
+        var setPrev = false;
+        if (initialValue) {
             prev = initialValue;
             setPrev = true;
         }
-        for(i = 0; i < length; i++) {
-            if(i in arr) {
-                if(setPrev) {
+
+        for (var i = 0; i < length; i++) {
+            if (i in arr) {
+                if (setPrev) {
                     prev = iterator.call(arr, prev, arr[i], i, arr);
                 } else {
-                    prev = arr[i]
+                    prev = arr[i];
                     setPrev = true;
                 }
             }
@@ -230,17 +357,5 @@ define(['koko/lang/generic'], function(generic) {
         return prev;
     };
 
-    return {
-        json: json,
-        object: object,
-        array: array,
-        fn: fn,
-        string: string,
-        trim: string.trim,
-        indexOf: array.indexOf,
-        lastIndexOf: array.lastIndexOf,
-        filter: array.filter,
-        forEach: array.forEach,
-        bind: fn.bind
-    };
+    return exports;
 });
