1c20ff0116da4387ff02d414ff379d1ec552cd22
mescoda <mescodasun@gmail.com>
2015-04-23 14:33:40 +0800

    rewrite event.on

--- src/event/event.js
+++ src/event/event.js
@@ -1,115 +1,112 @@
-define(['koko/dom/style'], function(domS) {
-    function on(elem, type, listener) {
-        if(elem && elem.addEventListener) {
+define(function (require) {
+    'use strict';
+
+    /**
+     * event of koko
+     *
+     * @exports event
+     */
+    var exports = {};
+
+    var generic = require('../lang/generic');
+    var _ = require('../lang/es5');
+    var domD = require('../dom/data');
+
+
+    var addEvent = function (elem, type, listener) {
+        if (elem.addEventListener) {
             elem.addEventListener(type, listener, false);
-        } else if(elem && elem.attachEvent) {
+        } else if (elem.attachEvent) {
             elem.attachEvent('on' + type, listener);
         }
-    }
-
-    function off(elem, type, listener) {
-        if(elem && elem.removeEventListener) {
-            elem.removeEventListener(type, listener, false);
-        } else if(elem && elem.detachEvent) {
-            elem.detachEvent('on' + type, listener);
-        }
-    }
-
-    function emit(elem, type) {
-        var event;
-        if(document.createEvent) {
-            event = document.createEvent('HTMLEvents');
-            event.initEvent(type, true, true);
-            elem.dispatchEvent(event);
-        } else {
-            event = document.createEventObject();
-            elem.fireEvent('on' + type, event);
-        }
-    }
-
-    function getTarget(e) {
-        e = e || window.event;
-        return e.target || e.srcElement;
-    }
-
-    function preventDefault(e) {
-        if(e.preventDefault) {
-            e.preventDefault();
-        } else {
-            e.returnValue = false;
-        }
-    }
-
-    function stopPropagation(e) {
-        if(e.stopPropagation) {
-            e.stopPropagation();
-        } else {
-            e.cancelBubble = true;
-        }
-    }
-
-    /**
-     * [getClientX description]
-     * @param  {Event} e event
-     * @return {Number}   clientX
-     */
-    function getClientX(e) {
-        var clientX = e.clientX;
-        if(clientX || clientX === 0) {
-            clientX -= domS.getCompatElement().clientLeft
-        }
-        return clientX;
-    }
-
-    /**
-     * [getClientY description]
-     * @param  {Event} e event
-     * @return {Number}   clientY
-     */
-    function getClientY(e) {
-        var clientY = e.clientY;
-        if(clientY || clientY === 0) {
-            clientY -= domS.getCompatElement().clientTop
-        }
-        return clientY;
-    }
-
-    /**
-     * [getPageX description]
-     * @param  {Event} e event
-     * @return {Number}   pageX
-     */
-    function getPageX(e) {
-        var pageX = e.pageX;
-        if(!pageX && pageX !== 0) {
-            pageX = getClientX(e) + domS.getCompatElement().scrollLeft;
-        }
-        return pageX;
-    }
-
-    /**
-     * [getPageY description]
-     * @param  {Event} e event
-     * @return {Number}   pageY
-     */
-    function getPageY(e) {
-        var pageY = e.pageY;
-        if(!pageY && pageY !== 0) {
-            pageY = getClientY(e) + domS.getCompatElement().scrollTop;
-        }
-        return pageY;
-    }
-
-    return {
-        on: on,
-        off: off,
-        emit: emit,
-        getTarget: getTarget,
-        preventDefault: preventDefault,
-        stopPropagation: stopPropagation,
-        getClientX: getClientX,
-        getClientY: getClientY,
-        getPageX: getPageX,
-        getPageY: getPageY
     };
+
+
+    var realEventHandler = function (elem, e) {
+        e = e || window.event;
+
+        // fix e.target
+        // identify the element on which the event occurred
+        if (!e.target) {
+            e.target = e.srcElement;
+        }
+
+        // fix e.currentTarget
+        // refer to the element the event handler has been attached
+        if (!e.currentTarget) {
+            e.currentTarget = elem;
+        }
+
+        // fix e.preventDefault
+        if (!e.preventDefault) {
+            e.preventDefault = function () {
+                e.returnValue = false;
+            };
+        }
+
+        // fix e.stopPropagation
+        if (!e.stopPropagation) {
+            e.stopPropagation = function () {
+                e.cancelBubble = true;
+            };
+        }
+
+        var currentTypeEvents = domD.get(elem, 'events')[e.type];
+
+
+        _.forEach(currentTypeEvents, function (event) {
+            event.call(elem, e);
+        });
+
+    };
+
+
+    exports.on = function (elem, type, handler) {
+
+        var elemDataPool = domD.get(elem);
+
+        // only add event on ELEMENT_NODE or DOCUMENT_NODE or window
+        // if nothing get, means elem is not above
+        if (!elemDataPool) {
+            return;
+        }
+
+
+        // get or init events object in elemDataPool
+        elemDataPool.events = elemDataPool.events || {
+            element: elem
+        };
+        var elemDataEvents = elemDataPool.events;
+
+
+        var currentTypeEvents = elemDataEvents[type];
+
+        if (!currentTypeEvents) {
+            // init current type data array
+            currentTypeEvents = elemDataEvents[type] = [];
+
+            var realHandler = generic.currying(realEventHandler, elem);
+
+            // bind the real event handler
+            // and each type on the same elem, the real handler will be bind only once
+            addEvent(elem, type, realHandler);
+        }
+
+        // add current handler to data pool
+        currentTypeEvents.push(handler);
+
+        // prevent memory leaks in IE
+        elem = null;
+    };
+
+    exports.off = function (elem, type, handler) {
+
+    };
+
+    exports.trigger = function (elem, type) {
+
+    };
+
+    return exports;
+
 });
